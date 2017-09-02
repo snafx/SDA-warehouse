b2c789454702be60bf347b1d78af2198f4ebfaa2
Bartek Mytko <bartlomiej.mytko@gmail.com>
2017-09-02 09:41:23 +0200

    Added JPA repositories for Product and Category entities

---
+++ src/main/java/com/sda/Warehouse/repositories/JpaCategoryRepository.java
@@ -0,0 +1,14 @@
+package com.sda.Warehouse.repositories;
+
+import com.sda.Warehouse.models.Category;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by mytek on 2017-09-02.
+ */
+public interface JpaCategoryRepository extends CrudRepository<Category, Integer> {
+
+    //interfejs, który dostarcza zbiór metoed związanych z create, read, update, delete dla klasy Category
+    //nie trzeba tworzyć implementacjitego interfejsu - Spring samgo dostarczy
+
+}
