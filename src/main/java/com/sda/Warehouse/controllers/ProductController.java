ea9d46142e4d0bd2bb1806a806843b5fbae1ad89
unknown <jakub.piechocki@gmail.com>
2017-09-05 20:25:24 +0200

    edit template and load product parameters

--- src/main/java/com/sda/Warehouse/controllers/ProductController.java
+++ src/main/java/com/sda/Warehouse/controllers/ProductController.java
@@ -1,5 +1,8 @@
 package com.sda.Warehouse.controllers;
 
+
+import com.sda.Warehouse.models.Product;
+
 import com.sda.Warehouse.repositories.JpaProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +20,7 @@ public class ProductController {
         this.jpaProductRepository = jpaProductRepository;
     }
 
+
     @GetMapping(value = "/product/{productId}")
     public ModelAndView singleProduct(@PathVariable("productId") Long productId) {
         ModelAndView modelAndView = new ModelAndView("product");
@@ -29,5 +33,18 @@ public class ProductController {
         ModelAndView modelAndView = new ModelAndView("product");
         modelAndView.addObject("product", jpaProductRepository);
         return modelAndView;
+    }
+
+    @GetMapping(path = "/product/{productId}/edit")
+    public ModelAndView loadProductParameters(@PathVariable("productId") Long productId) {
+        ModelAndView modelAndView = new ModelAndView("editproduct");
+        Product product = jpaProductRepository.findOne(productId);
+        modelAndView.addObject("product", product);
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/edit")
+    public String editStart() {
+        return "edit-product";
     }
 }
--- src/main/java/com/sda/Warehouse/repositories/JpaProductRepository.java
+++ src/main/java/com/sda/Warehouse/repositories/JpaProductRepository.java
@@ -3,9 +3,9 @@ package com.sda.Warehouse.repositories;
 import com.sda.Warehouse.models.Product;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by RENT on 2017-09-02.
- */
+import java.util.List;
+
+
 public interface JpaProductRepository extends CrudRepository<Product, Long> {
 
     //interfejs, który dostarcza zbiór metoed związanych z create, read, update, delete dla klasy Category
