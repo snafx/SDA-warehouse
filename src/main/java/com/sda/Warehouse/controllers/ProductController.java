3751500018e54acac5cd103cc252f18adec243be
Bartłomiej Mytko <bartlomiej.mytko@gmail.com>
2017-09-06 15:07:43 +0200

    Added sorting products by name or category name

--- src/main/java/com/sda/Warehouse/controllers/ProductController.java
+++ src/main/java/com/sda/Warehouse/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,14 +35,124 @@ public class ProductController {
         this.jpaCategoryRepository = jpaCategoryRepository;
     }
 
-    // strona w wszystkimi produktami; sciezka: "/products"
+    // strona w wszystkimi produktami; sciezka: "/products" lub "/products?page=3"
 
     @GetMapping
     public String allProducts(Model model,
-                              @RequestParam(value = "page", required=false, defaultValue = "1") String page,
-                              @RequestParam(value = "search-phrase", required=false, defaultValue = "") String searchPhrase,
-                              @RequestParam(value = "sort-by", required=false, defaultValue = "") String sortColumn,
-                              @RequestParam(value = "sort-type", required=false, defaultValue = "") String sortType) {
+                              @RequestParam(value = "page", required = false, defaultValue = "1") String page) {
+
+        System.out.println("JESTEM W OGOLNYM KONTROLERZE");
+
+        Integer productsPerPage = 5;
+        Integer firstPage = 1;
+        Integer currentPage = Integer.valueOf(page);
+
+        PageRequest myPageabble = new PageRequest(currentPage - 1, productsPerPage);
+
+        Page<Product> pageOfProducts = jpaProductRepository.findAll(myPageabble);
+
+
+        Long totalElements = pageOfProducts.getTotalElements();
+        Integer totalPages = pageOfProducts.getTotalPages();
+        List<Product> allProducts = pageOfProducts.getContent();
+
+        model.addAttribute("allProducts", allProducts);
+        model.addAttribute("totalElements", totalElements);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("firstPage", firstPage);
+        model.addAttribute("currentPage", currentPage);
+
+        return "products";
+    }
+
+    // strona w wyszukanymi produktami; sciezka: "/products?search-phrase=abc" lub "/products?search-phrase=abc&page=3"
+
+    @GetMapping(params = {"search-phrase"})
+    public String searchedProducts(Model model,
+                                   @RequestParam(value = "search-phrase") String searchPhrase,
+                                   @RequestParam(value = "page", required = false, defaultValue = "1") String page) {
+
+        System.out.println("JESTEM W KONTROLERZE OD WYSZUKIWANIA");
+
+        Integer productsPerPage = 5;
+        Integer firstPage = 1;
+        Integer currentPage = Integer.valueOf(page);
+
+        PageRequest myPageabble = new PageRequest(currentPage - 1, productsPerPage);
+
+        Page<Product> pageOfProducts = jpaProductRepository.findByNameIgnoreCaseContainingOrDescriptionIgnoreCaseContaining(searchPhrase.toUpperCase(), searchPhrase.toUpperCase(), myPageabble);
+
+
+        Long totalElements = pageOfProducts.getTotalElements();
+        Integer totalPages = pageOfProducts.getTotalPages();
+        List<Product> allProducts = pageOfProducts.getContent();
+
+        model.addAttribute("allProducts", allProducts);
+        model.addAttribute("totalElements", totalElements);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("firstPage", firstPage);
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("searchPhrase", searchPhrase);
+
+        return "products";
+    }
+
+    // strona posortowanymi produktami; sciezka: "/products?sort-by=kolumna&sort-type=asc/desc" lub "/products?sort-by=kolumna&sort-type=asc/desc&page=3"
+
+    @GetMapping(params = {"sort-by", "sort-type"})
+    public String sortedProducts(Model model,
+                                 @RequestParam(value = "sort-by", required = false, defaultValue = "") String sortColumn,
+                                 @RequestParam(value = "sort-type", required = false, defaultValue = "") String sortType,
+                                 @RequestParam(value = "page", required = false, defaultValue = "1") String page) {
+
+        System.out.println("JESTEM W KONTROLERZE OD SORTOWANIA");
+
+        Integer productsPerPage = 5;
+        Integer firstPage = 1;
+        Integer currentPage = Integer.valueOf(page);
+
+        Sort.Direction sortDirection = Sort.Direction.ASC;
+        if (sortType.equals("desc")) {
+            sortDirection = Sort.Direction.DESC;
+        }
+
+        String sortColumnName = "name";
+        if (sortColumn.equals("category")) {
+            sortColumnName = "category.name";
+        }
+
+        Sort.Order mySortOrder = new Sort.Order(sortDirection, sortColumnName);
+        Sort mySort = new Sort(mySortOrder);
+
+        PageRequest myPageabble = new PageRequest(currentPage - 1, productsPerPage, mySort);
+
+        Page<Product> pageOfProducts = jpaProductRepository.findAll(myPageabble);
+
+
+        Long totalElements = pageOfProducts.getTotalElements();
+        Integer totalPages = pageOfProducts.getTotalPages();
+        List<Product> allProducts = pageOfProducts.getContent();
+
+        model.addAttribute("allProducts", allProducts);
+        model.addAttribute("totalElements", totalElements);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("firstPage", firstPage);
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("sortColumn", sortColumn);
+        model.addAttribute("sortType", sortType);
+
+        return "products";
+    }
+
+
+    // strona w wszystkimi produktami; sciezka: "/products"
+/*
+    @GetMapping
+    public String allProducts(Model model,
+                              @RequestParam(value = "page", required = false, defaultValue = "1") String page,
+                              @RequestParam(value = "search-phrase", required = false, defaultValue = "") String searchPhrase,
+                              @RequestParam(value = "sort-by", required = false, defaultValue = "") String sortColumn,
+                              @RequestParam(value = "sort-type", required = false, defaultValue = "") String sortType) {
 
 //        Iterable<Product> allProducts = jpaProductRepository.findAll();
 
@@ -72,16 +183,5 @@ public class ProductController {
 
         return "products";
     }
-
-
-    @PostMapping(params = {"search-phrase"})
-    public String searchAllProducts(Model model, @RequestParam(value = "search-phrase") String searchPhrase) {
-
-//        List<Product> allProducts = jpaProductRepository.findByNameContainingOrDescriptionContaining(searchPhrase, searchPhrase);
-
-//        model.addAttribute("allProducts", allProducts);
-        model.addAttribute("searchPhrase", searchPhrase);
-
-        return "products";
-    }
+  */
 }
